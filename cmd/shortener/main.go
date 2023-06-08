ac49e4b2010eca3040de1929104544f42e8cc647
pizhlo <zaush@bk.ru>
2023-06-08 11:42:14 +0300

    using chi framework

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	internal "github.com/Pizhlo/yandex-shortener/internal/app"
+	"github.com/go-chi/chi"
 )
 
-func webhook(m internal.Model) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("webhook")
+// func webhook(m internal.Model) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Println("webhook")
 
-		if r.Method == http.MethodGet {
-			fmt.Println("MethodGet")
-			internal.GetURL(m, w, r)
-			return
-		} else if r.Method == http.MethodPost {
-			fmt.Println("MethodPost")
-			internal.ReceiveURL(m, w, r)
-			return
-		} else {
-			fmt.Println("StatusBadRequest")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-	}
+// 		if r.Method == http.MethodGet {
+// 			fmt.Println("MethodGet")
+// 			internal.GetURL(m, w, r)
+// 			return
+// 		} else if r.Method == http.MethodPost {
+// 			fmt.Println("MethodPost")
+// 			internal.ReceiveURL(m, w, r)
+// 			return
+// 		} else {
+// 			fmt.Println("StatusBadRequest")
+// 			w.WriteHeader(http.StatusBadRequest)
+// 			return
+// 		}
+// 	}
 
-}
+// }
 
 func main() {
 	if err := run(); err != nil {
@@ -36,9 +36,15 @@ func main() {
 }
 
 func run() error {
-	mux := http.NewServeMux()
 	m := make(internal.Model)
-	mux.HandleFunc(`/`, webhook(m))
 
-	return http.ListenAndServe(`:8080`, mux)
+	r := chi.NewRouter()
+	r.Get("/{id}", func(rw http.ResponseWriter, r *http.Request) {
+		internal.GetURL(m, rw, r)
+	})
+	r.Post("/", func(rw http.ResponseWriter, r *http.Request) {
+		internal.ReceiveURL(m, rw, r)
+	})
+
+	return http.ListenAndServe(":8080", r)
 }
