da9407c2fa59914e230fed66d4d72d1243baf69e
pizhlo <zaush@bk.ru>
2023-07-04 22:58:46 +0300

    added saving data to the file

--- config/config.go
+++ config/config.go
@@ -10,17 +10,20 @@ import (
 var baseAddrRegexp = regexp.MustCompile("^:[0-9]{1,}$")
 
 type Config struct {
-	FlagRunAddr  string
-	FlagBaseAddr string
-	FlagLogLevel string
+	FlagRunAddr    string
+	FlagBaseAddr   string
+	FlagLogLevel   string
+	FlagPathToFile string
+	FlagSaveToFile bool
 }
 
 func ParseConfigAndFlags() Config {
 	var conf Config
-	
+
 	flag.StringVar(&conf.FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&conf.FlagBaseAddr, "b", "http://localhost:8080", "base address for urls")
 	flag.StringVar(&conf.FlagLogLevel, "l", "info", "log level")
+	flag.StringVar(&conf.FlagPathToFile, "f", "/tmp/short-url-db.json", "file to save short urls")
 
 	flag.Parse()
 
@@ -41,9 +44,19 @@ func ParseConfigAndFlags() Config {
 		conf.FlagLogLevel = val
 	}
 
+	if val, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok {
+		conf.FlagPathToFile = val
+	}
+
+	if conf.FlagPathToFile != "" {
+		conf.FlagSaveToFile = true
+	} 
+
 	fmt.Println("FlagRunAddr = ", conf.FlagRunAddr)
 	fmt.Println("FlagBaseAddr = ", conf.FlagBaseAddr)
 	fmt.Println("FlagLogLevel = ", conf.FlagLogLevel)
+	fmt.Println("FlagFileStorage = ", conf.FlagPathToFile)
+	fmt.Println("FlagSaveToFile = ", conf.FlagSaveToFile)
 
 	return conf
 }
