3c6d3aa1d14eebab7f1e33fe4c899e3766545f21
pizhlo <zaush@bk.ru>
2023-08-02 11:37:57 +0300

    fixed session: fixed maketoken()

--- internal/app/session/session.go
+++ internal/app/session/session.go
@@ -48,21 +48,9 @@ func CookieMiddleware(next http.Handler) http.Handler {
 			}
 		}
 
-		// valid, err := validToken(cookie.Value)
-		// if err != nil {
-		// 	fmt.Println("CookieMiddleware validToken err = ", err)
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	return
-		// }
-
-		// if !valid { // if there was cookie but invalid
-		// 	userID := uuid.New()
-		// 	makeCookie(w, userID)
-		// }
-
 		if token != "" {
-            http.SetCookie(w, &http.Cookie{Name: "token", Value: token, HttpOnly: true})
-        }
+			http.SetCookie(w, &http.Cookie{Name: "token", Value: token, HttpOnly: true})
+		}
 
 		c := context.WithValue(context.WithValue(r.Context(), UserIDKey, userID), ValidOwnerKey, ownerValid)
 
@@ -107,85 +95,5 @@ func makeToken() (string, uuid.UUID, error) {
 		return "", uuid.UUID{}, err
 	}
 
-	return tokenString, uuid.New(), nil
+	return tokenString, userID, nil
 }
-
-// func makeCookie(w http.ResponseWriter, userID uuid.UUID) {
-// 	fmt.Println("makeCookie")
-// 	token, err := buildToken(userID)
-// 	if err != nil {
-// 		fmt.Println("makeCookie buildToken err = ", err)
-// 		w.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	cookie := &http.Cookie{
-// 		Name:     "token",
-// 		Value:    token,
-// 		HttpOnly: true,
-// 		Secure:   true,
-// 		Path:     "/",
-// 	}
-
-// 	http.SetCookie(w, cookie)
-// }
-
-// func buildToken(userID uuid.UUID) (string, error) {
-// 	fmt.Println("buildToken")
-// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
-// 		RegisteredClaims: jwt.RegisteredClaims{
-// 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(TokenExp)),
-// 		},
-// 		UserID: userID,
-// 	})
-
-// 	tokenString, err := token.SignedString([]byte(SecretKey))
-// 	if err != nil {
-// 		fmt.Println("buildToken SignedString err = ", err)
-// 		return "", err
-// 	}
-
-// 	return tokenString, nil
-
-// }
-
-// func validToken(tokenString string) (bool, error) {
-// 	fmt.Println("validToken")
-// 	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
-// 		if t.Valid {
-// 			return []byte(SecretKey), nil
-// 		}
-// 		return []byte(SecretKey), nil
-// 	})
-// 	if err != nil {
-// 		fmt.Println("validToken Parse err = ", err)
-// 		return false, err
-// 	}
-// 	if token.Valid {
-// 		return true, nil
-// 	}
-
-// 	return false, nil
-// }
-
-// func GetUserID(tokenString string) (uuid.UUID, error) {
-// 	fmt.Println("GetUserID")
-// 	claims := &Claims{}
-// 	token, err := jwt.ParseWithClaims(tokenString, claims,
-// 		func(t *jwt.Token) (interface{}, error) {
-// 			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-// 				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
-// 			}
-// 			return []byte(SecretKey), nil
-// 		})
-// 	if err != nil {
-// 		fmt.Println("GetUserID ParseWithClaims err = ", err)
-// 		return uuid.UUID{}, err
-// 	}
-
-// 	if !token.Valid {
-// 		return uuid.UUID{}, nil
-// 	}
-
-// 	return claims.UserID, nil
-// }
